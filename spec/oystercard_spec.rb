5eac5e227e5c4320be323fd1ec66e64eb7a1f0ea
Alison Woodman <aliwoodman@gmail.com>
2017-07-10 13:31:55 +0100

    test to check initial card balance is zero, passes

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,10 @@
 require 'oystercard'
 
 describe Oystercard do
+subject(:card) {described_class.new}
+
+  it 'checks a new card has balance of zero' do
+    expect(card.balance).to eq 0
+  end
 
 end
